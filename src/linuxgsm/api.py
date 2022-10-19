1691d2a4135bd768fef122702354a36460100600
Ben Neal <bneal16@gmail.com>
2022-10-18 22:50:51 -0400

    updates

--- src/linuxgsm/api.py
+++ src/linuxgsm/api.py
@@ -3,6 +3,7 @@ import os
 import docker
 import logging
 import tarfile
+import re
 
 
 def get_logger():
@@ -18,27 +19,41 @@ def patch_game_server(server_name):
     Copies server files to game server and restarts the server
 
     """
-    client = get_docker_client()
-    container = client.containers.get(server_name)
+    #client = get_docker_client()
+    #container = client.containers.get(server_name)
     logger = get_logger()
     
-    server_patch_path = os.getcwd(), server_name
+    server_patch_path = os.path.join(os.getcwd(), server_name)
     logger.info(f"Server patch path: {server_patch_path}")
-    if not os.path.exists(os.path.join()):
+    if os.path.exists(server_patch_path):
+        os.chdir(server_patch_path)
+
+        if server_name == 'pzserver':
+            source_file_path = os.path.join(server_patch_path, 'pzserver.txt')
+            transformed_file_path = os.path.join(server_patch_path, 'pzserver.ini')
+
+            with open(source_file_path, 'r') as file:
+                source_file_buffer = file.read()
+                transformed_file_buffer = source_file_buffer.replace("PZ_SERVER_PASSWORD", )
+                #print(source_file_buffer)
+
+            # with tarfile.open(server_patch_path + '.tar', mode='w') as tar:
+            #     try:
+            #         tar.add(srcname)
+            #     finally:
+            #         tar.close()
+
+    else:
         logger.error(f"Server {server_name} not found ")
 
-    os.chdir(server_patch_path)
-    # srcname = os.path.basename(src)
-    # tar = tarfile.open(src + '.tar', mode='w')
-    # try:
-    #     tar.add(srcname)
-    # finally:
-    #     tar.close()
+        # srcname = os.path.basename(src)
+        # tar = tarfile.open(src + '.tar', mode='w')
 
-    # data = open(src + '.tar', 'rb').read()
-    # container.put_archive(os.path.dirname(dst), data)
-    
-    # return container
+
+        # data = open(src + '.tar', 'rb').read()
+        # container.put_archive(os.path.dirname(dst), data)
+        
+        # return container
 
 
 def start_game_server(server_name):
